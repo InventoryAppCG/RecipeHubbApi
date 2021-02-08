19ae72dacf4e6879ce3db00b633fae80746de6bd
Gracias <gracias.claude@outlook>
2021-02-08 09:22:55 -0700

    updated routes to handle mongo with mongoose. Add user table / schema. Add post route to test adding to mongoAtlass. Add env for api key to mongo atlas.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
+const mongoose = require('mongoose')
 const express = require('express');
 const dotenv = require('dotenv') 
+const UserModel = require('./models/user.ts')
+const bodyParser = require('body-parser')
+// const UserModel = require('./models/user')
 
+mongoose.connect(
+  process.env.MONGO_API_KEY,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3333
+app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(port, () => {
   console.log(` Server is running at localhost:${port}`);
+})
+
+app.post('/create-user', (req, res) => {
+UserModel.create(req,res)
 })

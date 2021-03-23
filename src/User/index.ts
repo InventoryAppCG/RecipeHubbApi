f3725aa233657d49b2984c9994a6f48b27f60e6b
Gracias <gracias.claude@outlook>
2021-03-22 23:42:26 -0600

    add middleware for user routes

--- src/User/index.ts
+++ src/User/index.ts
@@ -3,6 +3,9 @@ const route = require('express').Router();
 const controller = require('./UserController')
 
 //middleware
+route.use(require('../Middleware/UserToken'))
+
+
 route.post('/', controller.create)
 route.get('/', controller.read)
 route.put('/:email', controller.update)

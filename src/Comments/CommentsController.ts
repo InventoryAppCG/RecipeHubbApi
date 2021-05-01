62dd600482bc26d6515b03332512f2485192d345
Gracias <gracias.claude@outlook>
2021-05-01 11:49:19 -0600

    added files

---
+++ src/Comments/CommentsController.ts
@@ -0,0 +1,47 @@
+export{}
+const { CommentModel } = require('../models/comments')
+
+module.exports = {
+    async get(req,res) {
+        try {
+        if(!req.params.id) {
+            throw Error("No recipeId provided")
+        }
+        let comments = await CommentModel.findOne({recipeId: req.params.id})
+        
+            //get ratings array
+            const ratings = comments.ratings
+
+            // get length of rating aray of objects
+            const length = comments.ratings.length
+
+            //adding rating value together
+            const total = ratings.reduce((accumulator, currentValue) => accumulator.rated + currentValue.rated)
+
+            // get the average
+            const average = total / length
+
+
+            // append average to the comments data
+            comments = {...comments._doc, average: average}
+
+            res.status(200).json(comments)
+        
+        } catch(err) {
+            console.log(err)
+            res.status(404).send(err)
+
+        }
+    },
+    async store(req, res) {
+        try {
+            const comments = await CommentModel.create(req.body)
+
+            res.status(200).json(comments)
+
+        } catch(err) {
+            console.log(err)
+            res.status(404).send(err)
+        }
+    }
+}

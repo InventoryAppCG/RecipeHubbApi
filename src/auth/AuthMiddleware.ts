42456818ab47fa5cef451e15e45c54fae1bf1912
gracias <gracias.claude@outlook.com>
2022-08-31 18:38:29 -0600

    gc/bug-auth - add eslint configuration and dependencies - fix spacing in commentController.ts. I need to updating spacing to all files

--- src/auth/AuthMiddleware.ts
+++ src/auth/AuthMiddleware.ts
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-    if (req.headers.authorization !== process.env.AUTHORIZATION) {
-        return res.send('Unauthorized')
-    }
-    //  res.header("Access-Control-Allow-Origin", req.header('origin'))x
-    return next()
-}
+  if (req.headers.authorization !== process.env.AUTHORIZATION) {
+    return res.send("Unauthorized");
+  }
+  //  res.header("Access-Control-Allow-Origin", req.header('origin'))x
+  return next();
+};
